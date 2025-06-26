c628d018a1f745632dfe672389e29c48b15f0ab2
bruna-tonelli <bc39081@gmail.com>
2025-06-26 09:52:54 -0300

    Create Tiriangulo.cs

---
+++ Aulas/Tiriangulo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo {
+    class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+
+
+
+    }
+}
+
